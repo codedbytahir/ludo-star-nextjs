19d843f1e034b207e30fdbaaa052e0c201ee62fc
Tahir Rasool <200578194+codedbytahir@users.noreply.github.com>
2026-01-16 17:34:40 +0500

    Update Next.js config for static export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [],
+    unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/ludo-star-nextjs' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ludo-star-nextjs/' : '',
 }
 
 module.exports = nextConfig
