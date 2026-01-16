7e72acfa802cf0825deef10a6fe353603a945617
Tahir Rasool <200578194+codedbytahir@users.noreply.github.com>
2026-01-16 18:25:04 +0500

    Fix Next.js config for proper deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ludo-star-nextjs' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ludo-star-nextjs/' : '',
 }
 
 module.exports = nextConfig
